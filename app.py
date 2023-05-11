a959a500ae3bca06a992e09cc191c354bc828a7b
RODRIGO WIRTHMANN TEIXEIRA <rodrigo.wteixeira@senacsp.edu.br>
2023-05-11 11:49:15 -0300

    Menus

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/base")
+def base():
+    return render_template("base.html")
+
 @app.route("/login")
 def login():
     return render_template("login.html")
@@ -18,6 +22,9 @@ def contato():
 def cursos():
     return render_template("cursos.html")
 
+@app.route("/EAD")
+def ead():
+    return render_template("ead.html")
 
 if __name__ == '__main__' :
     app.run(debug=True)    
